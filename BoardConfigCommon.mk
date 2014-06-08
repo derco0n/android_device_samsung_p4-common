c8cc84f34c4c95d9abbc43b220e6fe83abdb60fe
Daniel Tjandra <danieltjandra@gmail.com>
2014-06-08 18:10:18 -0400

    Initial kitkat commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,154 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Default value, if not overridden else where.
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/samsung/p4-common/bluetooth
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := false
+#USE_CAMERA_STUB := false
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS #flash compatibility
+TEGRACAP := true
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_CPU_VARIANT := tegra2
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+
+#Board info
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := 
+BOARD_PAGE_SIZE := 2048
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_BOOTLOADER_BOARD_NAME := p3
+
+#Graphics
+BOARD_EGL_NEEDS_LEGACY_FB := true
+#Screenshot, freeze and hwc fix from omni
+BOARD_EGL_SKIP_FIRST_DEQUEUE := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_NEEDS_OLD_HWC_API := true
+##################
+MAX_EGL_CACHE_KEY_SIZE := 4096
+MAX_EGL_CACHE_SIZE := 2146304
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+BOARD_EGL_CFG := device/samsung/p4-common/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+#camera also needed for HW decoding
+
+#TF101 camera stuff
+BOARD_USES_PROPRIETARY_LIBCAMERA := true
+BOARD_SECOND_CAMERA_DEVICE := true
+BOARD_CAMERA_HAVE_ISO := true
+USE_CAMERA_STUB := true
+ICS_CAMERA_BLOB := true
+BOARD_VENDOR_USE_NV_CAMERA := true
+
+#Sound
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
+HAVE_PRE_KITKAT_AUDIO_BLOB := true
+
+#CWM Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/p4-common/recovery/recovery_ui.c
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_INITRC := device/samsung/p4-common/recovery/init.rc
+
+# Indicate that the board has an Internal SD Card
+BOARD_HAS_SDCARD_INTERNAL := true
+
+# device-specific extensions to the updater binary
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/p4-common
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Wifi related defines
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_LEGACY_NL80211_STA_EVENTS := true
+
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan0 firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Use nicer font rendering
+BOARD_USE_SKIA_LCDTEXT := true
+
+# Charging Mode (LPM)
+BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/charging_mode_booting"
+
+# Custom graphics for recovery
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/p4-common/recovery/graphics.c
+
+# Preload bootanimation in to memory
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+BOARD_NEEDS_LOWFPS_BOOTANI := true
+
+# Suppress EMMC WIPE
+BOARD_SUPPRESS_EMMC_WIPE := true
+
+#skip dumb selinux metadata stuff
+SKIP_SET_METADATA := true
+
+#TWRP Flags
+TARGET_RECOVERY_FSTAB := device/samsung/p4-common/rootdir/fstab.p3
+HAVE_SELINUX := true
+DEVICE_RESOLUTION := 1280x800
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_ALWAYS_RMRF := true
+TW_FLASH_FROM_STORAGE := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_NO_EXFAT_FUSE := true
+TW_NO_EXFAT := true
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/cmc623_pwm_bl/backlight/pwm-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+#TW_USE_TOOLBOX := true #causes weirdness. do not use. kill supersu instead
+TW_NO_USB_STORAGE := true
+RECOVERY_FSTAB_VERSION := 2
+SP1_NAME := "efs"
+SP1_BACKUP_METHOD := files
+SP1_MOUNTABLE := 1
+TW_EXCLUDE_SUPERSU := true
---
+++ p4-common.mk
@@ -0,0 +1,165 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+PRODUCT_AAPT_CONFIG := xlarge mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Target arch
+TARGET_ARCH := arm
+
+TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_HEIGHT := 800
+
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/rootdir/init.p3.rc:root/init.p3.rc \
+    $(LOCAL_PATH)/rootdir/fstab.p3:root/fstab.p3 \
+    $(LOCAL_PATH)/rootdir/ueventd.p3.rc:root/ueventd.p3.rc \
+    $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc \
+	$(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/rootdir/init.p3.usb.rc:root/init.p3.usb.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    $(LOCAL_PATH)/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl \
+    $(LOCAL_PATH)/keylayout/sec_power_key.kl:system/usr/keylayout/sec_power_key.kl \
+    $(LOCAL_PATH)/keylayout/sec_keyboard.kl:system/usr/keylayout/sec_keyboard.kl \
+    $(LOCAL_PATH)/keylayout/Vendor_04e8_Product_7021.kl:system/usr/keylayout/Vendor_04e8_Product_7021.kl \
+    $(LOCAL_PATH)/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/gps.conf:system/etc/gps.conf
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/97random:system/etc/init.d/97random \
+     $(LOCAL_PATH)/99su:system/etc/init.d/99su
+
+# LPM (from TW-UX 3.2)
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/lpm/lib/libQmageDecoder.so:system/lib/libQmageDecoder.so \
+     $(LOCAL_PATH)/lpm/bin/lpmkey:system/bin/lpmkey \
+     $(LOCAL_PATH)/lpm/bin/playlpm:system/bin/playlpm \
+     $(LOCAL_PATH)/lpm/media/battery_charging_0.qmg:system/media/battery_charging_0.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_charging_20.qmg:system/media/battery_charging_20.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_charging_40.qmg:system/media/battery_charging_40.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_charging_60.qmg:system/media/battery_charging_60.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_charging_80.qmg:system/media/battery_charging_80.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_charging_100.qmg:system/media/battery_charging_100.qmg \
+     $(LOCAL_PATH)/lpm/media/battery_error.qmg:system/media/battery_error.qmg \
+     $(LOCAL_PATH)/lpm/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
+     $(LOCAL_PATH)/lpm/media/Disconnected.qmg:system/media/Disconnected.qmg
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15 \
+    media.stagefright.cache-params=6144/-1/30 \
+    ro.sf.lcd_density=160 \
+    dalvik.vm.dexopt-data-only=1 \
+    debug.hwui.render_dirty_regions=false \
+	ro.zygote.disable_gl_preload=true \
+	ro.bq.gpu_to_cpu_unsupported=1
+
+# Disable SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.selinux=disabled \
+	ro.build.selinux=0
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.root_access=1
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
+    $(LOCAL_PATH)/wifi/bcmdhd_p2p.bin:system/etc/wifi/bcmdhd_p2p.bin \
+    $(LOCAL_PATH)/wifi/bcmdhd_sta.bin:system/etc/wifi/bcmdhd_sta.bin
+
+PRODUCT_PACKAGES += \
+        libinvensense_mpl
+
+# Omni packages
+PRODUCT_PACKAGES += \
+        OmniTorch \
+        OmniSwitch
+# Audio
+PRODUCT_PACKAGES += \
+        audio.a2dp.default \
+		audio.usb.default \
+        libaudioutils \
+        libtinyalsa
+
+#make use of that wasted cache parition
+PRODUCT_PROPERTY_OVERRIDES += \
+		dalvik.vm.dexopt-data-only=0
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/audio/asound.conf:system/etc/asound.conf \
+        $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+        $(LOCAL_PATH)/audio/nvaudio_conf.xml:system/etc/nvaudio_conf.xml
+        
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/nvcamera.conf:system/etc/nvcamera.conf \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory \
+    power.p3 \
+    libnetcmdiface
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs
+
+DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/overlay
+
+# for bugmailer
+# removed, not present in cm10.2
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
+
