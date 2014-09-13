3f8221d1ddc2a174f29cf70071afa74cdd4527bf
RyanG <decatf@gmail.com>
2014-09-13 11:07:59 -0400

    boardconfig: Target cortex-a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
-TARGET_CPU_VARIANT := generic
+# TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a
