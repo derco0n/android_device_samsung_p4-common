5f3462e97ae2672bf740b814bca1b4a7a2beb03b
RyanG <decatf@gmail.com>
2015-03-01 13:42:28 -0500

    PowerHAL: Update for Lollipop
     - Low Power Mode power hint caps the maximum frequency
     - add boostpulse on Interaction hint
     - remove non-existent input_boost and boost_factor (gone since kernel 3.4)
     - refactor nonsensical logic in p3_power_set_interactive
     - lower go_hispeed_load on setInteractive on

--- power/power.c
+++ power/power.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #define LOG_TAG "P3 PowerHAL"
 #include <utils/Log.h>
@@ -26,16 +27,30 @@
 #include <hardware/power.h>
 
 #define SCALINGMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
-#define SCREENOFFMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq"
-#define BOOST_PATH      "/sys/devices/system/cpu/cpufreq/interactive/boost"
-static int boost_fd = -1;
-static int boost_warned;
+#define CPUFREQ_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/"
+// #define BOOST_PATH      "/sys/devices/system/cpu/cpufreq/interactive/boost"
+#define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+// #define HISPEED_FREQ "/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq"
+
+#define LOW_POWER_MAX_FREQ "456000"
+#define LOW_POWER_MIN_FREQ "150000"
+#define NORMAL_MAX_FREQ "1000000"
 
 #define MAX_BUF_SZ	10
 
 /* initialize to something safe */
 static char screen_off_max_freq[MAX_BUF_SZ] = "456000";
 static char scaling_max_freq[MAX_BUF_SZ] = "1000000";
+static char normal_max_freq[MAX_BUF_SZ] = "1000000";
+
+static bool low_power_mode = false;
+
+struct p3_power_module {
+    struct power_module base;
+    pthread_mutex_t lock;
+    int boostpulse_fd;
+    int boostpulse_warned;
+};
 
 int sysfs_read(const char *path, char *buf, size_t size)
 {
@@ -54,7 +69,7 @@ int sysfs_read(const char *path, char *buf, size_t size)
 	return len;
 }
 
-static void sysfs_write(char *path, char *s)
+static int sysfs_write(char *path, char *s)
 {
     char buf[80];
     int len;
@@ -63,83 +78,135 @@ static void sysfs_write(char *path, char *s)
     if (fd < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error opening %s: %s\n", path, buf);
-        return;
+        return -1;
     }
 
     len = write(fd, s, strlen(s));
     if (len < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error writing to %s: %s\n", path, buf);
+        return -1;
     }
 
     close(fd);
+    return 0;
 }
 
-static void p3_power_init(struct power_module *module)
+static void store_max_freq(char* max_freq)
+{
+    int len;
+    char buf[MAX_BUF_SZ];
+
+    /* read the current scaling max freq */
+    len = sysfs_read(SCALINGMAXFREQ_PATH, buf, sizeof(buf));
+
+    /* make sure it's not the screen off freq, if the "on"
+     * call is skipped (can happen if you press the power
+     * button repeatedly) we might have read it. We should
+     * skip it if that's the case
+     */
+    if (len != -1 && strncmp(buf, screen_off_max_freq,
+            strlen(screen_off_max_freq)) != 0
+            && !low_power_mode)
+        memcpy(max_freq, buf, sizeof(buf));
+}
+
+static void p3_power_init( __attribute__((unused)) struct power_module *module)
 {
     /*
      * cpufreq interactive governor: timer 20ms, min sample 30ms.
      */
 
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
-                "30000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "40000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
-                "80");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-		"0");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
-		"1");
+    sysfs_write(CPUFREQ_INTERACTIVE "timer_rate", "30000");
+    sysfs_write(CPUFREQ_INTERACTIVE "min_sample_time", "40000");
+    sysfs_write(CPUFREQ_INTERACTIVE "go_hispeed_load", "80");
 }
 
-static void p3_power_set_interactive(struct power_module *module, int on)
+static void p3_power_set_interactive( __attribute__((unused)) struct power_module *module, int on)
 {
-    int len;
-    char buf[MAX_BUF_SZ];
-
     /*
      * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
      * cpufreq policy.
      */
-
-    /* read the current scaling max freq */
-    len = sysfs_read(SCALINGMAXFREQ_PATH, buf, sizeof(buf));
     if (!on) {
-        /* save the current scaling max freq before updating */
-        if (len != -1)
-            memcpy(scaling_max_freq, buf, sizeof(buf));
+        store_max_freq(scaling_max_freq);
 
-        sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                    on ? scaling_max_freq : screen_off_max_freq);
+        sysfs_write(SCALINGMAXFREQ_PATH, screen_off_max_freq);
+        sysfs_write(CPUFREQ_INTERACTIVE "go_hispeed_load", "99");
+    } else if (low_power_mode) {
+        store_max_freq(scaling_max_freq);
+
+        sysfs_write(SCALINGMAXFREQ_PATH, LOW_POWER_MAX_FREQ);
+        sysfs_write(CPUFREQ_INTERACTIVE "go_hispeed_load", "99");
     } else {
-        sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                    on ? scaling_max_freq : screen_off_max_freq);
+        sysfs_write(SCALINGMAXFREQ_PATH, scaling_max_freq);
+        sysfs_write(CPUFREQ_INTERACTIVE "go_hispeed_load", "80");
+    }
+}
+
+static int boostpulse_open(struct p3_power_module *p3)
+{
+    char buf[80];
+
+    pthread_mutex_lock(&p3->lock);
+
+    if (p3->boostpulse_fd < 0) {
+        p3->boostpulse_fd = open(BOOSTPULSE_PATH, O_WRONLY);
+
+        if (p3->boostpulse_fd < 0) {
+            if (!p3->boostpulse_warned) {
+                strerror_r(errno, buf, sizeof(buf));
+                ALOGE("Error opening %s: %s\n", BOOSTPULSE_PATH, buf);
+                p3->boostpulse_warned = 1;
+            }
+        }
     }
 
-    if ((on) && (len = screen_off_max_freq))
-        sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq", scaling_max_freq);
-
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
-                on ? "1" : "0");
-
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
-		on ? "0" : "2");
-
+    pthread_mutex_unlock(&p3->lock);
+    return p3->boostpulse_fd;
 }
 
 static void p3_power_hint(struct power_module *module, power_hint_t hint,
                             void *data)
 {
+    struct p3_power_module *p3 =
+            (struct p3_power_module *) module;
     char buf[80];
-    int len;
+    int len, cpu, ret;
 
     switch (hint) {
     case POWER_HINT_VSYNC:
         break;
 
+#if 1
+    case POWER_HINT_INTERACTION:
+        if (boostpulse_open(p3) >= 0) {
+            len = write(p3->boostpulse_fd, "1", 1);
+
+            if (len < 0) {
+                strerror_r(errno, buf, sizeof(buf));
+                ALOGE("Error writing to %s: %s\n", BOOSTPULSE_PATH, buf);
+            }
+        }
+        break;
+#endif
+    case POWER_HINT_LOW_POWER:
+        pthread_mutex_lock(&p3->lock);
+        if (data) {
+            store_max_freq(normal_max_freq);
+
+            low_power_mode = true;
+            sysfs_write(SCALINGMAXFREQ_PATH, LOW_POWER_MAX_FREQ);
+            sysfs_write(CPUFREQ_INTERACTIVE "hispeed_freq", LOW_POWER_MAX_FREQ);
+        } else {
+            low_power_mode = false;
+            sysfs_write(SCALINGMAXFREQ_PATH, normal_max_freq);
+            sysfs_write(CPUFREQ_INTERACTIVE "hispeed_freq", NORMAL_MAX_FREQ);
+        }
+        pthread_mutex_unlock(&p3->lock);
+        break;
     default:
-            break;
+        break;
     }
 }
 
@@ -147,18 +214,23 @@ static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
-struct power_module HAL_MODULE_INFO_SYM = {
-    .common = {
-        .tag = HARDWARE_MODULE_TAG,
-        .module_api_version = POWER_MODULE_API_VERSION_0_2,
-        .hal_api_version = HARDWARE_HAL_API_VERSION,
-        .id = POWER_HARDWARE_MODULE_ID,
-        .name = "P3 Power HAL",
-        .author = "The Android Open Source Project",
-        .methods = &power_module_methods,
+struct p3_power_module HAL_MODULE_INFO_SYM = {
+    base: {
+        common: {
+            tag: HARDWARE_MODULE_TAG,
+            module_api_version: POWER_MODULE_API_VERSION_0_2,
+            hal_api_version: HARDWARE_HAL_API_VERSION,
+            id: POWER_HARDWARE_MODULE_ID,
+            name: "P3 Power HAL",
+            author: "The Android Open Source Project",
+            methods: &power_module_methods,
+        },
+        init: p3_power_init,
+        setInteractive: p3_power_set_interactive,
+        powerHint: p3_power_hint,
     },
 
-    .init = p3_power_init,
-    .setInteractive = p3_power_set_interactive,
-    .powerHint = p3_power_hint,
+    lock: PTHREAD_MUTEX_INITIALIZER,
+    boostpulse_fd: -1,
+    boostpulse_warned: 0,
 };
