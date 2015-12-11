565f647d2b872a60415a07f18784be7575642d66
RyanG <decatf@gmail.com>
2015-12-11 15:17:00 -0500

    macloader: Fix make file guard

--- macloader/Android.mk
+++ macloader/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(filter p4 p4wifi p4tmo p4vzw, $(TARGET_DEVICE)),)
+ifneq ($(filter p4 p4wifi p4tmo p4vzw, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
