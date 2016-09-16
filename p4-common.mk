d168c64879d35e81bd125690caa128ddf9b6e0cd
ryang <decatf@gmail.com>
2016-09-16 10:33:53 -0400

    p4-common: Use Quick compiler for boot image

--- p4-common.mk
+++ p4-common.mk
@@ -50,6 +50,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=wlan0 \
     wifi.supplicant_scan_interval=15 \
     ro.sf.lcd_density=160 \
+    dalvik.vm.image-dex2oat-flags=--compiler-backend=Quick \
     dalvik.vm.dex2oat-flags=--compiler-backend=Quick \
     debug.hwui.render_dirty_regions=false \
     ro.zygote.disable_gl_preload=true \
