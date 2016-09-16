fdba89e89b9d1e89ab5b6e09b640915cc2403348
ryang <decatf@gmail.com>
2016-09-16 12:30:58 -0400

    p4-common: bluetooth: enable ble

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,4 +22,8 @@
 /* kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
+
 #endif
