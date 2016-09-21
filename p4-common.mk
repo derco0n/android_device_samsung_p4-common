ee49aaf9c0b0850b97be2a29b38f63cb98c562bb
ryang <decatf@gmail.com>
2016-09-21 13:38:33 -0400

    libnvcompat: Compatibility symbols for ICS blobs

--- p4-common.mk
+++ p4-common.mk
@@ -172,7 +172,9 @@ PRODUCT_PACKAGES += \
     libdgv1 \
     libnvshim \
     libp4shim \
-    libp4utl
+    libp4utl \
+    libcompatgui \
+    libcompatbinder
 
 DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/overlay
