03513f000b400d48f6e8551690e45f2a5e617d87
ryang <decatf@gmail.com>
2017-09-28 10:11:12 -0400

    build as cortex-a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,8 +28,8 @@ WITH_DEXPREOPT := false
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a
-TARGET_CPU_VARIANT := generic
-# TARGET_CPU_VARIANT := cortex-a9
+# TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_NO_BOOTLOADER := true
