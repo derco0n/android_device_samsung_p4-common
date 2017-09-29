c59c2cf73c1309ebe1ebac1382c6872ff194a0cc
ryang <decatf@gmail.com>
2017-09-29 13:11:23 -0400

    libaudio: Move to vendor partition

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -5,6 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.tegra
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := audio_hw.c
 LOCAL_C_INCLUDES += \
