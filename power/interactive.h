c8d553baade595e668cd69b0bff72b59cb7cae3e
ryang <decatf@gmail.com>
2017-11-10 08:52:15 -0500

    power: enable interactive and interactive boost

--- power/interactive.h
+++ power/interactive.h
@@ -1,6 +1,8 @@
 #ifndef __INTERACTIVE_H__
 #define __INTERACTIVE_H__
 
+#define CPUFREQ_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/"
+
 void boost_on(struct p3_power_module *p3, void *data);
 void boostpulse(struct p3_power_module *p3);
 void boostpulse_init(struct p3_power_module *p3);
