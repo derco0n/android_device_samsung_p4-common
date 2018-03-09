8f2c89e224287090a62d7f51cf859d4cfaa3d54a
ryang <decatf@gmail.com>
2018-03-08 23:11:07 -0500

    p4-common: gnss: Move gpsd blob to init.rc service

--- gnss/service.cpp
+++ gnss/service.cpp
@@ -13,15 +13,6 @@ using android::hardware::defaultPassthroughServiceImplementation;
 
 int main() {
 
-    int pid = fork();
-
-    if ( pid == 0 ) {
-        int err;
-        err = execl("/system/vendor/bin/gpsd", "-c", "/system/vendor/etc/gps.xml", (char *)0);
-        if (err == -1) {
-            ALOGE("Error executing gpsd: %s", strerror(errno));
-        }
-    }
 
     // The GNSS HAL may communicate to other vendor components via
     // /dev/vndbinder
